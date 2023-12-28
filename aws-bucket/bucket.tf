a99a9e8062b8e3a0584eabe10a934b4842ccfbc2
samuelfo <oliveira.samuelfo@gmail.com>
2023-12-28 15:44:12 -0300

    teste

--- aws-bucket/bucket.tf
+++ aws-bucket/bucket.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "bucket" {
+<<<<<<< HEAD
     bucket = "bucket_curso_terraform"
+=======
+  bucket = "bucket_curso_terraform"
+>>>>>>> 508c848 (teste)
 }
--- aws-bucket/main.tf
+++ aws-bucket/main.tf
@@ -1,22 +1,30 @@
 #Arquivo para criação de um bucket na AWS
 
 terraform {
-    required_version = ">= 1.3.0"
+  required_version = ">= 1.3.0"
 
+<<<<<<< HEAD
     required_providers {
       aws = {
         source = "hashicorp/aws"
         version = "5.31.0"
       }
+=======
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+>>>>>>> 508c848 (teste)
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    default_tags {
-      tags = {
-        owner = "samuel_oliveira"
-        managed-by = "terraform"
-      }
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      owner      = "samuel_oliveira"
+      managed-by = "terraform"
     }
+  }
 }
